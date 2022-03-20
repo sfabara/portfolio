e7b38846df24eb286d59d59bd77b83f6183f27b7
Sebastian Fabara <sbs3128@live.com>
2022-03-20 00:37:49 -0400

    portfolio page added

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,7 +1,53 @@
 import React, { useState } from "react";
-import {ProjectImage, ProjectCard,PortfolioHeaderContainer, PortfolioContent, Header, SubHeader, Description, Button, ProjectContent, ProjectTitle, ProjectDescription } from "../Portfolio/styles/portfolioStyles";
+import { ProjectImage, ProjectCard, PortfolioHeaderContainer, PortfolioContent, Header, SubHeader, Description, Button, ProjectContent, ProjectTitle, ProjectDescription } from "../Portfolio/styles/portfolioStyles";
 
 export function Portfolio() {
+
+  const Card = ({ dir }) => {
+    if (dir == 'right')
+      return (
+
+        <ProjectCard>
+          <ProjectImage src="https://images.unsplash.com/photo-1555774698-0b77e0d5fac6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
+          <ProjectContent>
+            <ProjectTitle>Example Project</ProjectTitle>
+            <ProjectDescription>
+              My app does so much cool stuff it cooks and cleans for you and never ever lets you down. It stays positive for you always.
+              It also does other things and features like baking. What is the cause of all this destruction in Ukraine?
+            </ProjectDescription>
+            <Button>
+              <h2>
+                View Project
+              </h2>
+            </Button>
+          </ProjectContent>
+
+        </ProjectCard>
+      )
+    else if (dir == 'left')
+      return (
+        <ProjectCard>
+          <ProjectContent>
+            <ProjectTitle>Example Project</ProjectTitle>
+            <ProjectDescription>
+              My app does so much cool stuff it cooks and cleans for you and never ever lets you down. It stays positive for you always.
+              It also does other things and features like baking. What is the cause of all this destruction in Ukraine?
+            </ProjectDescription>
+            <Button>
+              <h2>
+                View Project
+              </h2>
+            </Button>
+          </ProjectContent>
+          <ProjectImage src="https://images.unsplash.com/photo-1555774698-0b77e0d5fac6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
+
+
+        </ProjectCard>
+      )
+  }
+
+
+
   return (
     <body>
 
@@ -16,31 +62,17 @@ export function Portfolio() {
           <h3>Design</h3>
         </SubHeader> */}
         <Description>
-          <p style={{textAlign: "left"}}>
-                Welome to the part where I show off all my skills applied to real world problems :D
+          <p style={{ textAlign: "left" }}>
+            Welome to the part where I show off all my skills applied to real world problems :D
           </p>
         </Description>
 
       </PortfolioHeaderContainer>
 
       <PortfolioContent>
-          <ProjectCard>
-                <ProjectImage src="https://images.unsplash.com/photo-1555774698-0b77e0d5fac6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"/>
-                <ProjectContent>
-                    <ProjectTitle>EXAMPLE PROJECT</ProjectTitle>
-                    <ProjectDescription>
-                        My app does so much cool stuff it cooks and cleans for you and never ever lets you down. It stays positive for you always.
-                        It also does other things and features like baking.
-                    </ProjectDescription>
-                    <Button>
-                        <h2>
-                            View Project
-                        </h2>
-                    </Button>
-                </ProjectContent>
 
-          </ProjectCard>
-         
+        <Card dir="left" />
+        <Card dir="right" />
       </PortfolioContent>
 
     </body>
--- src/components/Portfolio/styles/portfolioStyles.jsx
+++ src/components/Portfolio/styles/portfolioStyles.jsx
@@ -7,22 +7,18 @@ export const PortfolioHeaderContainer = styled.section`
     align-items: flex-start;
     justify-content: flex-start;
     flex-direction: column;
-    padding: 20rem;
+    padding: 20rem 20rem 10rem 20rem;
 
     & h1{
         font-size: 5rem;
         text-align: center;
     }
     & h3{
-        font-size: 2rem;
+        font-size: 3rem;
         font-weight: 100;
         font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif !important;
     }
-    & p {
-        font-size: 2rem;
-        margin: 0rem;
-        text-align: center;
-    }
+
 `
 export const PortfolioContent = styled.section`
     display: flex;
@@ -30,41 +26,54 @@ export const PortfolioContent = styled.section`
     justify-content: center;
     flex-direction: column;
     padding: 0 20rem 0 20rem;
+    flex-wrap:wrap;
 `
 
 export const ProjectCard = styled.div`
     display: flex;
     flex-direction: row;
     background-color: #00000020;
-    border-radius: 10px;
+    border-radius: 25px;
     width: 100%;
     padding: 2rem;
-    height: 39rem;
+    height: 35rem;
+    flex-wrap:wrap;
+    flex: 1;
+    margin: 0rem 0rem 20rem 0rem;
+    /* outline: 8px solid  #111318;
+    -moz-outline-radius: 16px;
+    box-shadow: 10px 10px 0px 20px #111318; */
 `
+
 export const ProjectImage = styled.img`
     display:flex;
     flex-direction: column;
-    width: 600px;
+    width: 40rem;
     padding: 2rem;
-
+    flex: 1;
+    border-radius: 25px;
 `
 export const ProjectContent = styled.div`
     display:flex;
     flex-direction: column;
     padding: 2rem;
+    flex: 1;
 
 `
 export const ProjectTitle = styled.h2`
     font-size: 3.2rem;
-    text-decoration: underline;
 `
 export const ProjectDescription = styled.p`
-    font-size: 3rem;
+    font-size: 2.5rem;
     font-weight: 100;
-    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif !important;
+    font-family: monospace !important;
     padding: 2.5rem 0 2.5rem 0;
+    height: 100%;
     `
 
+
+
+///Headers stuffs
 
 
 export const Header = styled.div`
@@ -97,33 +106,33 @@ padding-top: 1rem;
     & p {
         font-weight: 200;
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        font-size: 2.5rem;
+        font-size: 3rem;
     }
 
 `
 
-
 export const MyButton = styled(motion.div)`
     align-self: center;
-   
     display: flex;
     flex-direction: row;
     justify-content: space-around;
     align-items: center;
     background-color: ${(props) => props.theme.fancyButtonColor};
     padding: 20px;
-    width: 10rem;
+    width: 20rem;
     border-radius: 16px;
     outline: solid  #111318;
     -moz-outline-radius: 16px;
     box-shadow: 5px 5px 0px 4px #111318;
     cursor: pointer;
+    justify-items: stretch;
+
     &:hover{
       background-color: #0000004c;
     }
 
     & h2{
-        font-size: 1.6rem !important;
+        font-size: 1.9rem !important;
     }
 
 `;
